560cfe0a3670c046914f64acc1a601009bf2dceb
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-07 21:46:56 +0800

    feat: more styling

--- pages/election.js
+++ pages/election.js
@@ -26,7 +26,6 @@ const renderVotingResults = election => {
   const candidates = candidatesWithVotes(election).sort(
     (e1, e2) => e2.votes - e1.votes
   );
-  console.log(candidates);
   const renderedVotes = candidates.map((candidate, id) => (
     <div key={id} className="row text-center bg-light p-2">
       <div className="col-8">{candidate.title}</div>
@@ -50,6 +49,7 @@ const Page = ({ router }) => {
 
   const fetchElection = async id => {
     const election = await getElection(id);
+    console.log(election);
     setElection(election);
   };
 
